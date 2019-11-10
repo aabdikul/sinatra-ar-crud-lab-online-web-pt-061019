c7e70986db42b384d42cdd24c66a89158e89f39f
Aida Abdikulova <abdikulova@gmail.com>
2019-11-10 07:15:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,10 @@ class ApplicationController < Sinatra::Base
   get '/articles/new' do
     erb :new
   end
+
+  post '/articles' do 
+    @article = Article.create(:title => params[:title], :content => params[:content])
+    redirect to "/articles/#{@article.id}"
+  end
   
 end
